032670067711ea99de690552d023348ca314a1ef
Maria <idealniy.ringo@gmail.com>
2022-12-02 20:28:06 +0200

    added Procfile, added requirements, added TOKEN

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telebot
 import os
 
 app = Flask(__name__)
-TOKEN = os.environ.get('TOKEN')
+TOKEN = os.environ.get('5929783319:AAH2idFuxJeZb4UR7kpiFFsqGo5jIYpK-xY')
 bot = telebot.TeleBot(TOKEN)
 
 # 1. вітаємося і пропонуємо юзеру дії: кинути кубики або прочитати про клан інфу....
@@ -51,6 +51,20 @@ def red_cubes(red):
 
 .......................
 
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot 02-12-2022", 200
 
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://vtm-ve-bot.herokuapp.com/' + TOKEN)
+    return 'Python Telegram Bot', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
