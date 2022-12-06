6e3193630701ce3df611cdad87b317464bd6db81
Maria <idealniy.ringo@gmail.com>
2022-12-06 17:58:50 +0200

    app.run

--- VTMBot.py
+++ VTMBot.py
@@ -67,7 +67,6 @@ def get_message():
 
 
 if __name__ == '__VTMBot__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
 
 
-#host='0.0.0.0', port=int(os.environ.get('PORT', 8000))
