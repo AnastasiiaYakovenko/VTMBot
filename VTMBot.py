e0ea250329687abc0f8d0f36f2df1ed562ea6470
Maria <idealniy.ringo@gmail.com>
2022-12-06 18:23:27 +0200

    run

--- VTMBot.py
+++ VTMBot.py
@@ -66,7 +66,7 @@ def get_message():
 #     return 'Python Telegram Bot', 200
 
 
-if __name__ == '__VTMBot__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
+# if __name__ == '__VTMBot__':
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+#
 
